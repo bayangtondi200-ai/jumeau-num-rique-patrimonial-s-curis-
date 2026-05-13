e05a69912f49ad17406f42650a03678be2357da5
Armand BAYANG TONDI <bayangtondi200@gmail.com>
2026-05-13 16:45:55 +0100

    app/app.py (interface simple)

--- app/app.py
+++ app/app.py
@@ -1,1 +1,17 @@
+from flask import Flask, jsonify
+import json
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Digital Heritage Twin API is running"
+
+@app.route("/heritage")
+def heritage():
+    with open("../gis/sample_data.geojson") as f:
+        data = json.load(f)
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
