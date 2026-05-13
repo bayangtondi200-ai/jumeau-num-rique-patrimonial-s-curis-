143049064dcbc8d8584d34b45586bd3b78116e30
Armand BAYANG TONDI <bayangtondi200@gmail.com>
2026-05-13 16:42:24 +0100

    security/data_integrity.py
    Implemented a system for file integrity verification with SHA-256 hashing and modification detection.

--- security/data_integrity.py
+++ security/data_integrity.py
@@ -1,3 +1,28 @@
-Implémentation d’un système de vérification d’intégrité :
-- génération de hash
-- détection de modification
+
+import hashlib
+
+def generate_hash(file_path):
+    """Génère un hash SHA-256 pour un fichier"""
+    sha256 = hashlib.sha256()
+    
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            sha256.update(chunk)
+    
+    return sha256.hexdigest()
+
+
+def verify_integrity(file_path, original_hash):
+    """Vérifie si un fichier a été modifié"""
+    current_hash = generate_hash(file_path)
+    return current_hash == original_hash
+
+
+if __name__ == "__main__":
+    file = "../gis/sample_data.geojson"
+    
+    original = generate_hash(file)
+    print("Hash original :", original)
+
+    is_valid = verify_integrity(file, original)
+    print("Intégrité vérifiée :", is_valid)
